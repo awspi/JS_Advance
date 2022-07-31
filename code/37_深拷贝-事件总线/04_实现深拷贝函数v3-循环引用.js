7840c4f92d7e1dc55c6514ce179fc1e317ec6f87
awspi <206002328@qq.com>
2022-08-01 01:37:19 +0800

    update

--- code/37_深拷贝-事件总线/04_实现深拷贝函数v3-循环引用.js
+++ code/37_深拷贝-事件总线/04_实现深拷贝函数v3-循环引用.js
@@ -3,7 +3,8 @@ const isObject = (value) => {
   return value !== null && (valueType === "object" || valueType === "function")
 }
 //!递归调用
-function deepClone(originValue) {
+//* map在全局作用域无法被清除,在函数体内,递归后无法获取到上一次函数体的map,把map放在参数中初始化,后续递归时传入更新后的map
+function deepClone(originValue, map = new WeakMap()) {
   //?判断是否是Set类型
   if (originValue instanceof Set) {
     return new Set([...originValue])
@@ -23,10 +24,17 @@ function deepClone(originValue) {
   //?判断是否是对象类型
   if (!isObject(originValue)) return originValue
 
-  //*数组类型
+  //*如果已经存在key为originValue的value 就直接取出
+  if (map.has(originValue)) {
+    return map.get(originValue)
+  }
+  //?判断传入的是数组还是对象
   let newObject = Array.isArray(originValue) ? [] : {}
+  map.set(originValue, newObject)
+
+  //*clone
   for (const key in originValue) {
-    newObject[key] = deepClone(originValue[key])
+    newObject[key] = deepClone(originValue[key], map)
   }
   //*对symbol作为key进行特殊处理(for遍历不到symbol)
   const symbolKeys = Object.getOwnPropertySymbols(originValue)
@@ -34,7 +42,7 @@ function deepClone(originValue) {
     //*没必要再深拷贝sKey->symbol作为key是为了在同一个对象中使用相同的key,但是不同对象之间可以使用同一个的symbol
     // const newSKey = Symbol(sKey.description)
     // newObject[newSKey] = deepClone(originValue[sKey])
-    newObject[sKey] = deepClone(originValue[sKey])
+    newObject[sKey] = deepClone(originValue[sKey], map)
   }
 
   return newObject
@@ -74,9 +82,6 @@ const obj = {
 }
 
 const newObj = deepClone(obj)
-console.log(newObj === obj)
-console.log(newObj.s2 === obj.s2)
-
-obj.friend.name = "kobe"
-obj.friend.address.city = "成都"
-console.log(newObj)
+//循环引用
+newObj.info = newObj
+console.log(newObj.info.info)
--- code/37_深拷贝-事件总线/05_自定义事件总线.js
+++ code/37_深拷贝-事件总线/05_自定义事件总线.js
@@ -1,9 +1,9 @@
-class HYEventBus {
+class FakeEventBus {
   constructor() {
     this.eventBus = {}
   }
-
   on(eventName, eventCallback, thisArg) {
+    //TODO 检查参数类型
     let handlers = this.eventBus[eventName]
     if (!handlers) {
       handlers = []
@@ -11,48 +11,44 @@ class HYEventBus {
     }
     handlers.push({
       eventCallback,
-      thisArg
+      thisArg,
     })
   }
-
   off(eventName, eventCallback) {
     const handlers = this.eventBus[eventName]
     if (!handlers) return
-    const newHandlers = [...handlers]
-    for (let i = 0; i < newHandlers.length; i++) {
-      const handler = newHandlers[i]
+    const newHandler = [...handlers]
+    for (let i = 0; i < newHandler.length; i++) {
+      const handler = newHandler[i]
       if (handler.eventCallback === eventCallback) {
         const index = handlers.indexOf(handler)
         handlers.splice(index, 1)
       }
     }
   }
-
   emit(eventName, ...payload) {
     const handlers = this.eventBus[eventName]
     if (!handlers) return
-    handlers.forEach(handler => {
+    handlers.forEach((handler) => {
       handler.eventCallback.apply(handler.thisArg, payload)
     })
   }
 }
 
-const eventBus = new HYEventBus()
+const eventBus = new FakeEventBus()
 
-// main.js
-eventBus.on("abc", function() {
-  console.log("监听abc1", this)
-}, {name: "why"})
-
-const handleCallback = function() {
-  console.log("监听abc2", this)
+// TEST
+//*handleCallback
+const handleCallback1 = function () {
+  console.log("监听eventName|1", this, arguments)
 }
-eventBus.on("abc", handleCallback, {name: "why"})
-
-// utils.js
-eventBus.emit("abc", 123)
-
-// 移除监听
-eventBus.off("abc", handleCallback)
-eventBus.emit("abc", 123)
-
+const handleCallback2 = function () {
+  console.log("监听eventName|2", this, arguments)
+}
+//
+eventBus.on("eventName", handleCallback1, { name: "pithy" })
+eventBus.on("eventName", handleCallback2, { name: "awspi" })
+//
+eventBus.off("eventName", handleCallback1)
+//
+eventBus.emit("eventName", "arg1", "arg2")
